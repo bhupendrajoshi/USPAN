22d68fbeef5409511f3783cd1ca757571f2e3139
Bhupendra Joshi <bhupendra.joshi@gmail.com>
2017-07-14 21:44:57 +0200

    Moved web app in separate folder Add buyer home and manufacturer home components

---
+++ webapp/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/home/home';
+import BuyerHome from './components/buyerhome/buyerhome';
+import ManufacturerHome from './components/manufacturerhome/manufacturerhome';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/buyers" component={BuyerHome} />
+          <Route path="/manufacturers" component={ManufacturerHome} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+    );
+  }
+}
+
+export default App;
